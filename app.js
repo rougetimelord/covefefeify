0e24288eff72c846710a33ee9ff841085cf56f3a
Rouge <code@r0uge.org>
2019-08-23 00:14:44 -0700

    Update to run on a lot of text, simplify regexes

--- app.js
+++ app.js
@@ -1,20 +1,61 @@
-var g = function (s) {
-    return document.getElementById(s);
+var getElement = id => {
+    return document.getElementById(id);
 }
-var a = { 'b': 'p', 'c': 'g', 'd': 't', 'f': 'v', 'g': 'k', 'k': 'g', 'p': 'b', 's': 'z', 't': 'd', 'v': 'f', 'z': 's' }
-var y
-var c = function () {
-    var s=g('in').value,t=s.search(/[aeiou]/),i=s.slice(t+1),c=i.search(/[b-df-hj-np-tv-z]/),k=i.slice(c).search(/[aeiou]/),z=s.slice(0,t+c+2);
-    if(k==-1||c==-1){g('in').classList="f";return g('out').innerHTML="Autocorrect can't f*ck that up";}
-    k = i.slice(c)[k];
-    c=(typeof(a[i[c]])=='undefined')?i[c]:a[i[c]];
-    z+=(c+k).repeat(2);
-    g('in').classList='';g('out').innerHTML="That's <div>"+z+"</div> in 3AM twitter";
+var replacements = { 
+    'b': 'p', 
+    'c': 'g', 
+    'd': 't', 
+    'f': 'v', 
+    'g': 'k', 
+    'k': 'g', 
+    'p': 'b', 
+    's': 'z', 
+    't': 'd', 
+    'v': 'f', 
+    'z': 's' }
+var y = 0;
+var processText = () => {
+    var string = getElement('in').value;
+    var strings = string.split(' ');
+    var message = "";
+    strings.forEach(value => {
+        var match = value.match(/[aeiou][b-df-hj-np-tv-z]{1,}[aeiouy]/)
+        var out;
+        if (match === null) {
+            out = value;
+        }
+        else {
+            var first = value.substring(0, match.index + 2);
+            var con = first.substr(-1);
+            var vowel = match[0].substr(-1);
+            var conVowel = (typeof(replacements[con]) == 'undefined') ? con + vowel : replacements[con] + vowel;
+            out = first + conVowel.repeat(2);
+            console.log([match, first, con, vowel, conVowel, out])
+        }
+        message += out + " "
+    })
+    message = message.slice(0, -1);
+
+    if (message == string){
+        getElement('in').classList="f";
+        return getElement('out').innerHTML = "Autocorrect can't f*ck that up";
+    }
+    getElement('in').classList = '';
+    getElement('out').innerHTML = "That's <div>" + message + "</div> in 3AM twitter";
 }
-var key = function () {
-    clearTimeout(y);y = setTimeout(c, 500);
+var processTimeout = function () {
+    //Sets a timeout until to process the text
+    clearTimeout(y);
+    y = setTimeout(processText, 500);
 }
-var clear = function () {
-    if (g('in').value.length == 0){g('out').innerHTML="Translate normal language into autocorrect messing up"; cleartimeout(y);}
+var clearProcess = function () {
+    //Clears the timeout if a new character is added
+    if (getElement('in').value.length == 0) {
+        getElement('out').innerHTML = "Translate normal language into autocorrect messing up";
+        clearTimeout(y);
+    }
 }
-document.addEventListener('DOMContentLoaded', function () { document.addEventListener('keypress', key);document.addEventListener('keydown',clear)})
+document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener('keypress', processTimeout);
+    document.addEventListener('keydown', clearProcess);
+})
